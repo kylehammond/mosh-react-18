a2a9714e3d4db49294a7dfddc93b3e71f9945c0f
Kyle Hammond <kyle.hammond@gmail.com>
2023-11-02 13:10:01 -0500

    Styling components Exercise 2 solution

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,8 +1,8 @@
-import Button from "./components/Button";
+import Like from "./components/Like/Like";
 import "./App.css";
 
 function App() {
-  return <Button onClick={() => console.log("click")}>Click me</Button>;
+  return <Like onClick={() => console.log("clicked")} />;
 }
 
 export default App;
