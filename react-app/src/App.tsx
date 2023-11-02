d8b33d247b039b20fd226d97751ae06f7518fdf7
Kyle Hammond <kyle.hammond@gmail.com>
2023-11-02 13:10:01 -0500

    Styling components Exercise 1 solution

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,24 +1,8 @@
-import ListGroup from "./components/ListGroup";
+import Button from "./components/Button";
 import "./App.css";
-import { BsFillCalendarFill } from "react-icons/bs";
 
 function App() {
-  let items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
-  return (
-    <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
-      <BsFillCalendarFill color="red" size="40" />
-    </div>
-  );
+  return <Button onClick={() => console.log("click")}>Click me</Button>;
 }
 
 export default App;
