89ddd499b393030c9599b188b6c8a9541c640a61
Kyle Hammond <kyle.hammond@gmail.com>
2023-11-02 13:10:03 -0500

    updating state exercise (my sol)

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,31 +1,22 @@
 import { useState } from "react";
 import "./App.css";
-import produce from "immer";
 
 function App() {
-  const [bugs, setBugs] = useState([
-    { id: 1, title: "Bug 1", fixed: false },
-    { id: 2, title: "Bug 2", fixed: false },
-  ]);
+  const [game, setGame] = useState({
+    id: 1,
+    player: {
+      name: "John",
+    },
+  });
 
   const handleClick = () => {
-    // setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
-    setBugs(
-      produce((draft) => {
-        const bug = draft.find((bug) => bug.id === 1);
-        if (bug) bug.fixed = true;
-      })
-    );
+    let newGame = { ...game, player: { ...game.player, name: "Fred" } };
+    setGame(newGame);
   };
 
   return (
     <div>
-      {bugs.map((bug) => (
-        <p key={bug.id}>
-          {bug.title} {bug.fixed ? "Fixed" : "New"}
-        </p>
-      ))}
-      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick}>{game.player.name}</button>
     </div>
   );
 }
