c23a7501fb41a772a0c81bcb8e935c4c7911d88c
Kyle Hammond <kyle.hammond@gmail.com>
2023-11-21 15:06:36 -0600

    expense tracker - expense list (his sol)

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
 import "./App.css";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 function App() {
-  return <></>;
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
+  ]);
+
+  return (
+    <div>
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
+    </div>
+  );
 }
 
 export default App;
